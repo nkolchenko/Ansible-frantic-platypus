d3c12cdba15202388ea9f492713758fba21fab73
nkolchenko <nkolchenko@gmail.com>
2020-04-04 20:36:45 +0200

    Initial commit

---
+++ hello-go/cmd/hello/hello.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+// HelloServer responds to requests with the given URL path
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello from ServerGo. You requested: %s \n", r.URL.Path)
+    log.Printf(": Received request for path: %s", r.URL.Path)
+}
+
+func main() {
+    var addr string = ":8180"
+    handler := http.HandlerFunc(HelloServer)
+    if err := http.ListenAndServe(addr, handler); err != nil {
+        log.Fatalf("Could not listen on port %s %v", addr, err)
+    }
+}
