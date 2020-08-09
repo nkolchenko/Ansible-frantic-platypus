ffb1892b208d6bfcb9d908e6c7514ba082470a6a
nkolchenko <nkolchenko@gmail.com>
2020-08-09 17:33:20 +0200

    changes for 'do_GET' and 'construct_body' methods

--- hello-py/server.py
+++ hello-py/server.py
@@ -29,9 +29,9 @@ def srv_run():
 
 def calculate_dates(birth_date, curr_date):
     this_year_bdate = datetime(curr_date.year, birth_date.month, birth_date.day)
-    print("this year b-day: "+ str(this_year_bdate))
+    print("calculate_dates:   this year b-day: "+ str(this_year_bdate))
     next_year_bdate = datetime(curr_date.year+1, birth_date.month, birth_date.day)
-    print("next year b-day: "+ str(next_year_bdate ))
+    print("calculate_dates:   next year b-day: "+ str(next_year_bdate ))
     if curr_date > this_year_bdate:
         days_untill_bday = (next_year_bdate - curr_date).days
     else:
@@ -39,29 +39,36 @@ def calculate_dates(birth_date, curr_date):
 
     return days_untill_bday
 
-#curr_date = date.today()
-curr_date_str = datetime.strftime(date.today(), '%Y-%m-%d')
-curr_date = datetime.strptime(curr_date_str, '%Y-%m-%d')
-print("curr_date: " + str(curr_date))
+def get_days(birth_date_str):
+    #birth_date_str = '1984-08-10'
+    #curr_date = date.today()
+    curr_date_str = datetime.strftime(date.today(), '%Y-%m-%d')
+    curr_date = datetime.strptime(curr_date_str, '%Y-%m-%d')
+    print("get_days:   curr_date: " + str(curr_date))
 
-birth_date = datetime.strptime(birth_date_str, '%Y-%m-%d')
-print("birth_date: " + str(birth_date))
+    birth_date = datetime.strptime(birth_date_str, '%Y-%m-%d')
+    print("get_days:   birth_date: " + str(birth_date))
 
-days_untill_bday = calculate_dates(birth_date, curr_date)
+    days_untill_bday = calculate_dates(birth_date, curr_date)
 
-"""
+    """
             A. if username's birthday is in N days:
                 { "message": "Hello, <username>! Your birthday is in N day(s)" }
             B. if username's birthday is today:
                 { "message": "Hello, <username>! Happy birthday!" }
-"""
+    """
 
-print("+++")
-if days_untill_bday > 0:
-    print("Hello, <username>! Your birthday is in " + str(days_untill_bday) + " day(s)")
-else:
-    print("Hello, <username>! Happy birthday!")
+    return days_untill_bday
 
+
+def construct_body(username, days_untill_bday):
+
+    if days_untill_bday > 0:
+        message = "Hello, "+str(username)+"! Your birthday is in " + str(days_untill_bday) + " day(s)"
+    else:
+        message = "Hello, "+str(username)+"! Happy birthday!"
+    print("construct_body:   message: "+str(message))
+    return message
 
 
 #Handler = http.server.SimpleHTTPRequestHandler
@@ -69,9 +76,9 @@ else:
 
 class SputHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
     from urllib.parse import urlparse
-    def send_reply(self, srv_reply):
+    def send_reply(self, message):
         #message = name
-        print("sendHead: "+str(srv_reply))
+        print("send_reply:   got message: "+str(message))
 
         self.send_response(HTTPStatus.OK)
         #self.send_response(201, " DONE ")
@@ -84,7 +91,7 @@ class SputHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
         #body = None
 
 
-        body = srv_reply.encode('UTF-8', 'replace')
+        body = message.encode('UTF-8', 'replace')
         self.send_header("Content-type", "text/html")
         self.send_header('Content-Length', str(len(body)))
         self.end_headers()
@@ -122,10 +129,10 @@ class SputHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
         """
         bits = urlparse(self.path)
         #print(self.path)        # /hello/nikolay/uuuuer
-        username = self.path.split("/", 2)
-        print("got_username: "+str(username))              #  ['', 'hello', 'nikolay/uuuuer']
-        print("transformed: "+str(username[-1]))          #  nikolay/uuuuer
-        username = username[-1]
+        path_split = self.path.split("/", 2)
+        print("do_GET:   path_split: "+str(path_split))              #  ['', 'hello', 'nikolay/uuuuer']
+        username = path_split[-1]
+        print("do_GET:   username: " + str(path_split[-1]))  # nikolay/uuuuer
         #print(name, self.path, self.command, self.request_version, bits.scheme, bits.netloc,
          #                bits.path, bits.params, bits.query, bits.fragment,
          #                bits.username, bits.password, bits.hostname, bits.port)
@@ -137,15 +144,18 @@ class SputHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
                 { "message": "Hello, <username>! Happy birthday!" }
         """
 
-        srv_response = username
+        # TODO: get_data_from_mysql(username)                       -->  get  birth_date_str  for username
+
+        days_untill_bday = get_days(birth_date_str)
+        message = construct_body(username, days_untill_bday)
+        #message = username
 
         #self.send_response(HTTPStatus.OK)
-        self.send_reply(srv_response)
+        self.send_reply(message)
 #        self.send_header("Content-type", ctype)
 
 
-# TODO: check how SimpleHTTPRequestHandler.do_GET is done.
 
 if __name__ == '__main__':
+    print("Let's start web-server")
     srv_run()
-    print("22")
